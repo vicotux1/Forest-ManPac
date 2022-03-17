a8fb36c2755f1e5c39105a57fa43cfddc8b2ce21
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2022-03-16 21:05:22 -0300

    0.3beta
    Healt and enemys create an scripting

---
+++ Assets/scripts/Healt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healt : MonoBehaviour
+{
+    [SerializeField] string TagPlayer="Jugador";
+    [SerializeField] int Value=10;
+    [SerializeField] bool IsEnemy;
+
+    void OnTriggerEnter(Collider other){
+		if (other.tag == TagPlayer){
+      if(IsEnemy=false){
+          Destroy (this.gameObject);
+      }
+    
+        GameManager.Hearts+=Value;
+        Debug.Log(GameManager.Hearts);
+        }
+      } 
+
+      void Awake(){
+          // GameManager.Coins++;   
+      } 
+
+}
