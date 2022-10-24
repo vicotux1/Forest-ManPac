a5656b3b8580e95c91cf6af59b1dc6bd4a291705
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2022-10-24 12:27:58 -0300

    URP-Vulkan-Linear

--- Assets/Forest_Guard/Scripts/Player/Controller.cs
+++ Assets/Forest_Guard/Scripts/Player/Controller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour{
-    
+    #region Variables
     [SerializeField] Transform MeshPlayer;
     [SerializeField][Range(0.001f, 20.0f)]float Speed=0.1f, Gravity,SpeedRun;
     [SerializeField] string Horizontal, Vertical, Run;
@@ -13,8 +13,9 @@ public class Controller : MonoBehaviour{
     private  CharacterController _controller;
     float Axis_Horizontal, Axis_Vertical;
     Animator _anim;
-
-    private void Start(){
+    #endregion
+    #region Unity_Methods
+    private void Awake(){
         _controller=GetComponent<CharacterController>();
         _anim=gameObject.GetComponent<Animator>();
         Cursor.visible = true; 
@@ -23,46 +24,37 @@ public class Controller : MonoBehaviour{
        }
      
     void Update(){
-        
+        Movement();      
+    } 
+    #endregion
+
+    #region Movement
+        void Movement(){   
         Axis_Horizontal=Input.GetAxis(Horizontal);
         Axis_Vertical=Input.GetAxis(Vertical);
-        Movement(Axis_Horizontal, Axis_Vertical); 
-        //_Run();      
-    } 
-       void _Run(){
-       if(Input.GetButton(Run)){
-                Speed=SpeedRun;
-                Debug.Log(Speed);
-            }else{ Speed=Velocidad;
-            Debug.Log(Speed);
-        }
-    }    
-
-        void Movement(float Horizontal, float Vertical){
-                        
-            //Movement
-            Vector3 move=new Vector3(Vertical,-Gravity,Horizontal);
-            //rotate transform
-            Vector3 Rotation=new Vector3 (move.x,0, move.z);
-
-            if(Input.GetButton(Run)){
-                _controller.Move(move * Time.deltaTime * SpeedRun);
-             _anim.SetBool("Runner", true); 
-                }else{ _controller.Move(move * Time.deltaTime * Speed);
-                _anim.SetBool("Runner",false); 
-             _anim.SetFloat("Speed", Axis_Vertical); 
-        }
-
+        Vector3 Move=new Vector3(Axis_Vertical,-Gravity,Axis_Horizontal);
             
-            
-            if (Horizontal!=0 || Vertical!=0){
-                
-               _anim.SetBool("Rotate", true); 
-                //rotate transform
-            MeshPlayer.rotation= Quaternion.LookRotation(Rotation);
-            }else{
-               _anim.SetBool("Rotate", false);  
+            if(Input.GetButton(Run)){               
+                _anim.SetBool("Runner", true); 
+                _anim.SetFloat("IsRunV", Axis_Vertical);
+                _anim.SetFloat("IsRunH", Axis_Horizontal); 
+                _controller.Move(Move * Time.deltaTime * SpeedRun); 
             }
+            else{ 
+                _anim.SetBool("Runner",false); 
+                _anim.SetFloat("SpeedV", Axis_Vertical); 
+                _anim.SetFloat("SpeedH", Axis_Horizontal);
+                _controller.Move(Move * Time.deltaTime * Speed);  
+        }
+        Rotate(Move);      
     } 
-    
+    void Rotate(Vector3 move){
+        //rotate transform
+        Vector3 Rotation=new Vector3 (move.x,0, move.z);
+         if (Axis_Horizontal!=0 || Axis_Vertical!=0){
+          MeshPlayer.rotation= Quaternion.LookRotation(Rotation);
+          
+        }
+    }
+    #endregion
 }
